6617cdc0d5f574ebf8e131e7c007e6f4f45ab0a1
wuliang <wuliang@qccr.com>
2017-09-11 16:50:09 +0800

    ####

--- loganalysis/redis_action.py
+++ loganalysis/redis_action.py
@@ -3,7 +3,7 @@ import redis
 r = redis.Redis(host='127.0.0.1')
 
 def search_key(search_date):
-    return r.keys(pattern="%*s"%(search_date))
+    return r.keys(pattern="%s*"%(search_date))
 
 def get_redis(key):
     return r.get(key)
