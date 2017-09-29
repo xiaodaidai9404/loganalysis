6bff8ae15f6773b5d79846345afbc5f6b219b507
wuliang <wuliang@qccr.com>
2017-09-29 16:31:44 +0800

    ####

--- loganalysis/urls.py
+++ loganalysis/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from loganalysis.view import index,country_loganalysis,country_get,http_code_get,http_code_loganalysis,test,slow_url_loganalysis,slow_url_get,slow_url_domain
+from alert.view import alert_user
 
 urlpatterns = [
     url(r'^$', index),
@@ -26,5 +27,6 @@ urlpatterns = [
     url(r'^get/httpcode/$',http_code_get),
     url(r'^loganalysis/slowurl/$',slow_url_loganalysis),
     url(r'^get/slowurl/$',slow_url_get),
-    url(r'^get/domain/$',slow_url_domain)
+    url(r'^get/domain/$',slow_url_domain),
+    url(r'$alert/user/$',alert_user),
 ]
